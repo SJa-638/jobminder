cd7cb7c2ec330820f511505b1c9ba971e9884cd7
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2023-08-30 10:42:46 +0300

    Add companies Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jobminder/modules/application.dart';
 import 'package:jobminder/modules/company.dart';
 import 'package:jobminder/screens/application_details_screen.dart';
+import 'package:jobminder/screens/company_screen.dart';
 
 import 'blocs/applications_detailes/applications_detailes_bloc.dart';
 
@@ -30,27 +31,67 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   
+  final List<Company> companies = [
+    Company(name: 'Company 1'),
+    Company(name: 'Company 2'),
+    Company(name: 'Company 3'),
+    Company(name: 'Company 4'),
+    Company(name: 'Company 5'),
+    Company(name: 'Company 6'),
+    Company(name: 'Company 7'),
+    Company(name: 'Company 8'),
+    Company(name: 'Company 9'),
+    Company(name: 'Company 10'),
+    Company(name: 'Company 11'),
+    Company(name: 'Company 12'),
+    Company(name: 'Company 13'),
+    Company(name: 'Company 14'),
+    Company(name: 'Company 15'),
+    Company(name: 'Company 16'),
+    Company(name: 'Company 17'),
+    Company(name: 'Company 18'),
+    Company(name: 'Company 19')];
+
   final Application app = Application('test', WorkModle.remote, JobType.fulltime, Company(name: 'Company demo'));
-  
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: const Text("Fetch Data"),
-          onPressed: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => 
-                  BlocProvider(
-                    create: (context) => ApplicationDetailsBloc(),
-                    child: ApplicationDetailsScreen(application: app),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text("Show app details"),
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => 
+                      BlocProvider(
+                        create: (context) => ApplicationDetailsBloc(),
+                        child: ApplicationDetailsScreen(application: app),
+                      ),
                   ),
-              ),
-            )
-          },
+                )
+              },
+            ),
+            ElevatedButton(
+              child: const Text("Show Companies"),
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => 
+                      BlocProvider(
+                        create: (context) => ApplicationDetailsBloc(),
+                        child: CompaniesScreen(companies: companies),
+                      ),
+                  ),
+                )
+              },
+            ),
+          ],
         ),
       )
     );
---
+++ lib/screens/company_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../modules/company.dart';
+
+class CompaniesScreen extends StatefulWidget {
+  final List<Company> companies;
+
+  const CompaniesScreen({
+    super.key,
+    required this.companies,
+  });
+
+  @override
+  State<CompaniesScreen> createState() => _CompaniesScreenState();
+}
+
+class _CompaniesScreenState extends State<CompaniesScreen> {
+  // late ApplicationDetailsBloc bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    // bloc = context.read<ApplicationDetailsBloc>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Companies")),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: ListView.builder(
+            itemCount: widget.companies.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  print(widget.companies[index].name);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Card(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(widget.companies[index].name),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
